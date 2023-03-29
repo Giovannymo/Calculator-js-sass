81d5e2cacb4622482b8420b5f784f51622de4f9d
Giovanny Moreno <giovannysocas@gmail.com>
2023-03-29 11:56:11 -0500

    add: button reset and delete works now

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ import Calculadora from './calculator.js';
 
 const $keyboard = document.querySelector('.container__keyboard');
 const $input = document.querySelector('.container__input-number');
-const $reset = document.querySelector('.keyboard__keycap-reset');
 
 $keyboard.addEventListener('click', pressButton);
 
@@ -13,44 +12,23 @@ $keyboard.addEventListener('click', pressButton);
 
 function pressButton(e){
   e.preventDefault();
-  const calculadora = new Calculadora(2,3);
-
   const btnSelect = e.target.classList[1];
+  optionSelect(btnSelect)
 
-  if ($input.value.length >= 9) {
-    return;
-  }
-  
-  console.log(btnSelect)
-  if(btnSelect == 'keyboard__keycap-reset'){
-    return $input.value = '0'
+}
 
-  }
 
-  switch(btnSelect){
+function optionSelect(selection){
+ 
 
-    case 'keyboard__keycap-0':
-      return $input.value += '0';
-    case 'keyboard__keycap-1':
-      return $input.value += '1';
-    case 'keyboard__keycap-2':
-      return $input.value += '2'; 
-    case 'keyboard__keycap-3':
-      return $input.value += '3'; 
-    case 'keyboard__keycap-4':
-      return $input.value += '4'; 
-    case 'keyboard__keycap-5':
-      return $input.value += '5'; 
-    case 'keyboard__keycap-6':
-      return $input.value += '6'; 
-    case 'keyboard__keycap-7':
-      return $input.value += '7'; 
-    case 'keyboard__keycap-8':
-      return $input.value += '8'; 
-    case 'keyboard__keycap-9':
-      return $input.value += '9'; 
+
+  //Operation keycaps
+  switch(selection){
     case 'keyboard__keycap-del':
-      return $input.value += '1'; 
+      let input = Array.from($input.value)
+      input.pop()
+      let newValue = input.join('')
+      return $input.value = newValue
     case 'keyboard__keycap-plus':
       return $input.value += '1'; 
     case 'keyboard__keycap-rest':
@@ -64,11 +42,35 @@ function pressButton(e){
     case 'keyboard__keycap-reset':
       return $input.value = ''; 
     case 'keyboard__keycap-enter':
-      return $input.value += '1'; 
-    default:
-    console.log('ejecutado')
+      return $input.value += '1';  
   }
 
-
+  //It know keycap press
+  if($input.value.length <= 9 ){
+    
+    switch(selection){
+      case 'keyboard__keycap-0':
+        return $input.value += '0';
+      case 'keyboard__keycap-1':
+        return $input.value += '1';
+      case 'keyboard__keycap-2':
+        return $input.value += '2'; 
+      case 'keyboard__keycap-3':
+        return $input.value += '3'; 
+      case 'keyboard__keycap-4':
+        return $input.value += '4'; 
+      case 'keyboard__keycap-5':
+        return $input.value += '5'; 
+      case 'keyboard__keycap-6':
+        return $input.value += '6'; 
+      case 'keyboard__keycap-7':
+        return $input.value += '7'; 
+      case 'keyboard__keycap-8':
+        return $input.value += '8'; 
+      case 'keyboard__keycap-9':
+        return $input.value += '9'; 
+    
+    }
+  }
 
 }
